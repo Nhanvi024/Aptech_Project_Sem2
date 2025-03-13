aa31f211a6766269e66c9b6ecfc5272461a6f70a
DNTHoang <166272415+DNTHoang@users.noreply.github.com>
2025-03-13 13:36:06 +0700

    Update add-post.blade.php

--- resources/views/back/pages/post/admin/add-post.blade.php
+++ resources/views/back/pages/post/admin/add-post.blade.php
@@ -297,7 +297,7 @@
 
 
 	{{-- jQuery libary --}}
-	{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
@@ -306,7 +306,7 @@
 	<!-- Bootstrap JS -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 	<!-- Bootstrap Tags Input JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script> --}}
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
 
 	<script>
 		postImage.onchange = evt => {
